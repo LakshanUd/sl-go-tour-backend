9067ff076c574e4ea685f4ac790ffff6534e1696
LakshanUd <it23756250@my.sliit.lk>
2025-08-30 15:47:49 +0530

    Make functions Async 🖋

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,80 +1,60 @@
 import Vehicle from "../models/vehicle.js";
 
-export function createVehicle(req, res) {
-
+export async function createVehicle(req, res) {
+  try {
     const vehicle = new Vehicle(req.body);
-
-    vehicle.save()
-        .then((savedVehicle) => {
-            res.status(201).json({
-                message: "Vehicle created successfully"
-        })
-        })
-        .catch((error) => {
-            res.status(400).json({ error: error.message });
-        });
+    await vehicle.save();
+    res.status(201).json({ message: "Vehicle created successfully"});
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-export function getVehicles(req, res) {
-    Vehicle.find()
-        .then((vehicles) => {
-            res.status(200).json(vehicles);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: error.message });
-        });
+export async function getVehicles(req, res) {
+  try {
+    const vehicles = await Vehicle.find();
+    res.status(200).json(vehicles);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-export function getVehicleById(req, res) {
-
-    Vehicle.findOne({ 
-        productID: req.params.productID
-     }).then(
-        (vehicle) => {
-            res.status(200).json(vehicle)
-        }
-        ).catch(
-            (error) => {
-                res.status(404).json({
-                    message: error
-                })
-            }
-        )
+export async function getVehicleById(req, res) {
+  try {
+    const vehicle = await Vehicle.findOne({ productID: req.params.productID });
+    if (!vehicle) {
+      return res.status(404).json({ message: "Vehicle not found" });
+    }
+    res.status(200).json(vehicle);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-export function updateVehicle(req, res) {
-
-    Vehicle.findOneAndUpdate({
-        productID: req.params.productID
-        }, req.body).then(
-            () => {
-                res.status(200).json({
-                    message: "Vehicle updated successfully"
-                })
-            }
-        ).catch(
-            (error) => {
-                res.status(400).json({
-                    message: error
-                })
-            }
-        )
+export async function updateVehicle(req, res) {
+  try {
+    const updatedVehicle = await Vehicle.findOneAndUpdate(
+      { productID: req.params.productID },
+      req.body,
+      { new: true }
+    );
+    if (!updatedVehicle) {
+      return res.status(404).json({ message: "Vehicle not found" });
+    }
+    res.status(200).json({ message: "Vehicle updated", updatedVehicle });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-export function deleteVehicle(req, res) {
-    Vehicle.findOneAndDelete({
-        productID: req.params.productID
-    }).then(
-        () => {
-            res.status(200).json({
-                message: "Vehicle deleted"
-            })
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                message: error
-            })
-        }
-    )
+export async function deleteVehicle(req, res) {
+  try {
+    const deletedVehicle = await Vehicle.findOneAndDelete({ productID: req.params.productID });
+    if (!deletedVehicle) {
+      return res.status(404).json({ message: "Vehicle not found" });
+    }
+    res.status(200).json({ message: "Vehicle deleted"});
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
