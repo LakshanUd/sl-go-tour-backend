8506af54b76760d24bb732eda2ff0c10c58eccba
poornigamage2003 <poornigamage03@gmail.com>
2025-09-02 17:11:36 +0530

    fixed errors of Accommodation crud

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import vehicleRouter from "./routes/vehicleRouter.js";
 import InventoryRoutes from "./routes/InventoryRoutes.js";
 import cors from "cors";
+import AccommodationRoute from "./routes/AccommodationRoute.js";
 
 let app = express();
 app.use(cors());
@@ -14,7 +15,7 @@ app.use(bodyParser.json());
 
 app.use("/api/vehicles", vehicleRouter);
 app.use("/api/inventory", InventoryRoutes);
-
+app.use("/api/accommodations", AccommodationRoute);
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
