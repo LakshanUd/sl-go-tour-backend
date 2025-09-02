f432b6efdac844b93dd1eecb39accdb2e6d3a36b
jayanishehara2003 <jayanishehara29@gmail.com>
2025-09-02 15:43:29 +0530

    fix error of  three cruds (complaint/feedback/meal)

---
+++ models/ComplaintModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const complaintSchema = new mongoose.Schema(
+  {
+    name: { 
+      type: String 
+   },
+
+    email: { 
+      type: String 
+   },
+
+    service: { 
+      type: String, 
+      required: true 
+   },
+
+    category: { 
+      type: String, 
+      required: true 
+   },
+
+    description: { 
+      type: String, 
+      required: true 
+   },
+
+    status: { 
+      type: String,
+       default: "Open" 
+      },   
+  },
+  { timestamps: true }
+);
+
+const Complaint = mongoose.model("ComplaintModel", complaintSchema);
+export default Complaint;
