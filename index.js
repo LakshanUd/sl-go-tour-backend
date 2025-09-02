d9222e0fff120fdb930f5cd87599bdde075a9595
LakshanUd <it23756250@my.sliit.lk>
2025-09-02 11:22:56 +0530

    Change  port number 🚢

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use(bodyParser.json());
 
 app.use("/api/vehicles", vehicleRouter);
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
 })
